a399ff97519f601c1c7fdaa2df448cf414049845
Sergio Suarez <ssaurez3089@gmail.com>
2018-10-04 15:58:01 +0200

    cambio index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//Create a new omponent. This comptonent should produce
+//some HTML
+const App = function() {
+  return <div>Hi!</div>
+}
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Take this compronent HTML and put it
+//on the page (in the DOM)
